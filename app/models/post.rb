1127fa4126d3164848f8d498e01e6c18b9229a02
nicmackenzie <nicmackenzie@live.com>
2023-08-17 14:10:19 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, { presence: true }
+    validates :content, { length: { minimum: 250 } }
+    validates :summary, { length: { maximum: 250 } }
+    validates_inclusion_of :category, in: ['Fiction', 'Non-Fiction']
+    validate :sufficiently_clickbait_y
+
+    # def title_is_clickbaity
+    #     unless(title.downcase.include? "" )
+    #         error.add(:title,"Title not clickbaity")
+    #     end
+    # end
+
+    # private
+
+    private
+
+    def sufficiently_clickbait_y
+        required_phrases = ["Won't Believe", "Secret", /^Top \d+$/, "Guess"]
+    
+        unless required_phrases.any? { |phrase| title&.match?(phrase) }
+          errors.add(:title, "must be sufficiently clickbait-y")
+        end
+      end
+
 end
